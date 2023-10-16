fd785a66c30a9cf96284291421a6c4873a999c11
PokeRen <103824765+RenSnek@users.noreply.github.com>
2023-10-16 10:57:09 +0100

    Fixed createCanvasButton params

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-import * as canvasui from "./canvasui.js"
+import * as canvasui from "./canvasui.js";
 
-canvasui.createCanvasButton("test",{x:10,y:10,w:100,y:50},"Hello","red")
+canvasui.createCanvasButton("test",{x:10,y:10,w:100,h:50},"Hello","red");
 
 function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
