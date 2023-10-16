cf306433484d7f71b38acd07a68e69691ce47ab2
PokeRen <103824765+RenSnek@users.noreply.github.com>
2023-10-16 10:31:52 +0100

    Made canvas fit screen

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const ctx = canvas.getContext("2d");
 
 import * as canvasui from "./canvasui"
 
-function draw() {
+alert(canvasui.x);
 
+function draw() {
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.drawRect(0,0,canvas.width,canvas.height)
 }
 
 function gameLoop() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     draw();
     requestAnimationFrame(gameLoop);
 }
