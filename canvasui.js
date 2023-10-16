82b3faf0b973718c8b623cc74e477325c9fbdc75
PokeRen <103824765+RenSnek@users.noreply.github.com>
2023-10-16 10:52:22 +0100

    Created CanvasButton class and related functions

--- canvasui.js
+++ canvasui.js
@@ -1,1 +1,29 @@
-export const x = "Hello World"
+var canvasButtons = {};
+
+class CanvasButton {
+    constructor(id,pos,text,colour) {
+        this.id = id;
+        this.pos = pos;
+        this.text = text;
+        this.colour = colour;
+    }
+
+    draw(ctx) {
+        var col = ctx.fillStyle;
+        ctx.fillStyle = this.colour;
+        ctx.fillRect(this.pos.x,this.pos.y,this.pos.w,this.pos.h);
+        ctx.fillStyle = col;
+    }
+}
+
+function createCanvasButton(id,pos,text,colour) {
+    canvasButtons.id = new CanvasButton(id,pos,text,colour);
+}
+
+function drawCanvasButtons(ctx) {
+    for (var canvasButton of canvasButtons) {
+        canvasButton.draw(ctx);
+    }
+}
+
+export { createCanvasButton, drawCanvasButtons }
--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const ctx = canvas.getContext("2d");
 
 import * as canvasui from "./canvasui.js"
 
+canvasui.createCanvasButton("test",{x:10,y:10,w:100,y:50},"Hello","red")
+
 function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+    
+    canvasui.drawCanvasButtons(ctx);
+
 }
 
 function gameLoop() {
