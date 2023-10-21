27c0175c4bf102cca26b246fb5a343503f4999ed
PokeRen <103824765+RenSnek@users.noreply.github.com>
2023-10-21 16:25:37 +0100

    Fixed bug

--- tetrisboard.js
+++ tetrisboard.js
@@ -89,7 +89,7 @@ function rotateGridClockwise(matrix) {
     const y = n - 1;
     for (let i = 0; i < x; i++) {
        for (let j = i; j < y - i; j++) {
-          k = matrix[i][j];
+          var k = matrix[i][j];
           matrix[i][j] = matrix[y - j][i];
           matrix[y - j][i] = matrix[y - i][y - j];
           matrix[y - i][y - j] = matrix[j][y - i]
