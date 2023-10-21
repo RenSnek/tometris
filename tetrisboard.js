4b40e25b197179081d318dfd2825f39686ec614e
PokeRen <103824765+RenSnek@users.noreply.github.com>
2023-10-21 15:03:11 +0100

    Fixed tileAtBottom check

--- tetrisboard.js
+++ tetrisboard.js
@@ -84,8 +84,7 @@ function update() {
         var row = fallingBlockType[i];
         for(var j = 0; j < row.length; j++) {
             if (row[j] > 0) {
-                alert(j)
-                var tileAtBottom = ( j + fallingBlockCoords[1] > boardHeight )
+                var tileAtBottom = ( i + fallingBlockCoords[1] > boardHeight )
                 if (!tileAtBottom) {
                     var tileAboveTile = (board[i+fallingBlockCoords[0]][j+fallingBlockCoords[1]+1] > 0)
                 } else {
